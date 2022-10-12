a4661e2388b94189ab73eed0e2e46f0055c6951e
mduddin15 <rakibuddin.md@gmail.com>
2022-10-12 16:19:02 +0600

    PracticeforLoop

---
+++ src/PracticeforLoop.java
@@ -0,0 +1,11 @@
+public class PracticeforLoop {
+
+        public static void main(String[] args) {
+
+            // print numbers from 1 to 100 by using for loop.
+            for (int i = 0; i <= 100; i++) {
+                System.out.println(i);
+            }
+        }
+
+}
