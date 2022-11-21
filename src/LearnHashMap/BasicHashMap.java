e73d515c9c1c48d059ef77cedfd14585fab06d9a
mduddin15 <rakibuddin.md@gmail.com>
2022-11-21 20:14:59 +0600

    More practice on HashMap

--- src/LearnHashMap/BasicHashMap.java
+++ src/LearnHashMap/BasicHashMap.java
@@ -19,6 +19,25 @@ import java.util.HashMap;
             String x= RollNumber.get(4);
 
             System.out.println(x);
+            System.out.println(RollNumber.containsKey(4));
+            System.out.println(RollNumber.containsValue("Rin"));
+            System.out.println(RollNumber.containsValue("Rakib"));
+
+            // override a value and update with current value
+            RollNumber.put(1, "jonus");
+
+            // use of replace.
+            // override a value if it exists otherwise it dones not change anything
+            RollNumber.replace(4, "karim");
+            RollNumber.replace(8, "rahim");
+
+            // use of putIfAbsent.
+            // put the value if the key is absent.
+            RollNumber.putIfAbsent(5, "karim");
+            RollNumber.putIfAbsent(7, "rahim");
+
+            System.out.println(RollNumber);
+
 
             System.out.println(RollNumber.size());
 
